cf6a25d5787dfc8d78259f91786690c4629ad888
divyasonla <163973310+divyasonla@users.noreply.github.com>
2024-05-04 10:34:28 +0530

    Add files via upload

---
+++ mod4.js
@@ -0,0 +1,496 @@
+const input=require("readline-sync")
+/*let text="20";
+let text1="5"
+let result=text<text1;
+ console.log(result);*/
+
+
+/*let text="john";
+let text1="mano";
+let result=text<text;
+console.log(result);*/
+
+/*let text="john";
+let text1="doe";
+let text2=text+""+text1;
+console.log(text2);*/
+
+/*let text1="what a very"
+text1+=" nice day"
+console.log(text1);*/
+
+/*let x=5;
+if(x===5){
+    console.log("yes");
+}else{
+console.log("no");}*/
+
+/*let x=5;
+if(x==5)
+{
+    console.log("yes")
+}
+else{console.log("no")}*/
+
+/*let x=6;
+let y=3;
+if(x<10&&y>1)
+{
+    console.log("yes");
+}
+else{console.log("no");}*/
+
+/*let x=5;
+let y=4;
+if(x>3||y>8){
+    console.log("yes");
+}
+else{console.log("no");}*/
+
+/*let x=5;
+let y=4;
+if(!x==y)
+{
+    console.log("yes");
+}
+{console.log("no");}*/
+
+/*if("2">"12")
+{
+    console.log("yes");
+}
+else{
+    console.log("no");
+}*/
+
+// let name = null;
+// let text = "missing";
+// let result = name +" is "+ text;
+// console.log(result);
+
+// let a=5;
+// let b=4;
+// console.log(a+b);//addition//
+
+// let a=10;
+// let b=5;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a++);
+// console.log(a--);
+
+// let a=5;
+// let b=10;
+// let c="5";
+// let x=a
+// console.log(a==c);//true
+// console.log(a===c);//false
+// console.log(a==x);//true
+// console.log(a!=b);//true
+// console.log(a>b);//false
+// console.log(a<b);//true
+// console.log(a>=b);//false
+// console.log(a<=b);//true
+// console.log(a>=c);//true
+// console.log(a<=c);//true
+
+
+
+// let x = 5, y = 10, z = 15;
+
+// console.log(x = y);//10
+// console.log( x+= 1);//11
+// console.log(x -= 1);//10
+// console.log(x *= 5);//50
+// console.log(x /= 5);//10
+// console.log(x %= 2);//0
+
+// let a=5;b=10;
+// console.log((a!=b)&&(a<b));//true
+// console.log((a>b)||(a==b));//false
+// console.log((a<b)||(a==b));//true
+// console.log(!(a<b));//false
+// console.log(!(a>b));//true
+
+
+// let name ="divya sonla"
+// let name1="sharda"
+// let name2="sohan"
+// let name3="bhandari"
+// console.log(name.length,"\n",name1.length,"\n",name2.length,"\n",name3.length);
+
+//////SWITCH CASES//////////////////////////////////////////////////
+/*let day = "sunday";
+
+switch(day) {
+    case 'monday':
+      console.log("5 am");
+      break;
+    case 'tuesday':
+      console.log("6 am");
+      break;
+    case 'wednesday':
+      console.log("7 am");
+      break;
+    case 'thursday':
+      console.log("7 am");
+      break;
+    case 'friday':
+      console.log("7 am");
+      break;
+    case 'saturday':
+      console.log("8 am"); 
+      break;
+    case 'sunday':
+      console.log("7 am");
+      break;
+    // default:
+    //   console.log("Unknown day"); 
+  }
+  */
+
+  /*let name ="anita";
+  switch(name)
+  {
+    case'divya':
+        console.log("$$$$");
+        break;
+        case'anita':
+        console.log("#####");
+        break;
+        case 'urmila':
+        console.log("@@@@@");
+        break;
+        // default:
+            console.log("unknow person");
+  }*/
+
+
+
+//   let i=1;
+//   for(; ;)
+//   {
+//     console.log("*");
+//     i++
+//   }
+
+/*let n=input.questionInt("enter")
+var count=1;
+while(count<n)
+{
+    console.log(count);
+    count+=2
+}*/
+
+/*let n=input.questionInt("enter")
+var i=0;
+while(i<n)
+{
+    if(i%2==0)
+    {
+        console.log(i);
+    }
+    i++;
+}*/
+
+/*let n=input.questionInt("enter")
+for(let i=0;i<n;i++)
+{
+    console.log(i);
+}*/
+
+/*let n=input.questionInt("enter:")
+for(let i=0;n>i;n--)
+{
+    console.log(n);
+}*/
+
+/*for(i=25;i<=30;i++)
+{
+    console.log(i);
+}*/
+
+/*var i=0;
+do{
+    if(i%2==0)
+    {
+        console.log(i);
+    }
+    i++;
+}
+while(i<=10)*/
+
+//  let letters=[2,4,5,6,8]
+// for(const letter of letters)
+// {
+//   console.log(lettter);
+// }
+
+// let letters = ["4", "9", "6", "7"];
+
+// for (const letter of letters) {
+//     console.log(letter);
+// }
+
+// let greeting = "navgurukul";
+// for (const char of greeting) {
+//   console.log(char);
+// }
+
+/*let v=0
+  do{
+   console.log(v)
+
+}
+
+while(v<=3)
+v++;*/
+
+// number_list=[23,"maan",67,"gouri",90,45,34]
+// for(let num of number_list){
+// console.log(num);
+// }
+
+/*let arr=[2,3,4,5,6,7,8]
+for(let num in arr)
+{
+  console.log(num);
+}*/
+ 
+
+/*let Navgurukul = ['Pune', 'Bangalore', 'Sarjapur'];
+
+for (let cmpKey in Navgurukul) {
+ console.log(cmpKey);
+}
+console.log("--------------")
+for (let cmpValue of Navgurukul) {
+ console.log(cmpValue)
+}*/
+
+
+////////////////////////////////////
+// {
+//   var x=5
+// }
+// if(x<3){
+//   console.log(x,"yes");
+// }
+// else{
+//   console.log("no");
+// }
+// console.log(x);
+/////////////////////////////////////
+//   let x=5
+
+// {
+//   let x=5
+// }
+// if(x==5)
+// {
+//   console.log("yes");
+// }
+// else{console.log("no");}
+// console.log(x);
+/////////////////////////////////////////////////////
+// const x=5
+// {
+//   const x=5
+// }
+// if(x>3)
+// {
+//   console.log("yes");
+// }
+// else{console.log("no");}
+// console.log(x);
+
+ 
+
+
+
+/////////////////////////////////////////////////////
+
+
+// let m="apple"
+// let n="pplea"
+// // let greeting = "navgurukul";
+// for (const char of n) {
+//   console.log(char);
+// }
+// console.log(n.length);
+// let a=[]
+// let b=[]
+// for(i=0;i<n.length;i++)
+// {
+// a[i]=n[i];
+
+
+// // console.log(a);
+// for(let j=0;j<m.length;j++)
+// {
+//   b[j]=m[j];
+
+
+// // console.log(b);
+
+// if(b[j]==a[i])
+// {
+//   console.log("yes");
+//   let k=i;
+//   while(k<n.length)
+//   {
+//     a[k]=a[k+1];
+//     k+=1
+//   }
+  
+
+// }
+// // else j++
+// }
+// }
+
+// // {
+// //   let x=6;
+// //   console.log(x);
+
+// // }
+// // {
+// //   const x=6;
+// //   console.log(x);
+
+// // }  
+// // {
+// //   var y=0;
+// // }  console.log(y);
+
+
+
+// let name='divya';
+// for(char of name)
+// {
+//   console.log(char);
+
+// }
+// let name1 ='yadiv'
+// for(char in name1)
+
+// {
+//   console.log(char);
+// }
+// arr=[]
+
+
+//  if(name==name1)
+//  {
+//   console.log('yes');
+//  }
+//  else{
+//   console.log('no');
+//  }
+
+const int=require("readline-sync")
+let n=int.question("enter a name:");
+let m=int.question("enter a 2 name:");
+let a=[];
+let b=[];
+let pr=0
+if(n.length==m.length)
+{
+  let l=n.length;
+  for(let i=0;i<l;i++)
+  {
+    a[i]=n[i];
+    b[i]=m[i];
+  }
+  for(let i=0;i<l;i++)
+  {
+    let ll=l;
+    for(j=0;j<ll;j++)
+    {
+      if(a[i]==b[j])
+      {
+        while(j<ll)
+        {
+          b[j]=b[j+1]
+          j++
+        }
+        ll--
+        pr=1
+      }
+    }
+    if(pr==0)
+    {
+      console.log("no");
+      break;
+    }
+
+  }
+  if (pr==1) 
+  {
+    console.log("yes");
+  }
+  else{
+    console.log("no");
+  }
+}
+// const int=require("readline-sync")
+// let a=int.question("enter a name:");
+// let b=int.question("enter a 2 name:");
+// let arr=[];
+// let brr=[];
+// for(let i=0;i<a.length;i++)
+// {
+//   arr[i]=a[i]
+// }
+// for(j=0;j<b.length;j++)
+// {
+//   brr[j]=b[j]
+// }
+// console.log(arr,brr);
+
+// let c=[];
+// // let i=0
+// for(let i=0;i<arr.length;i++)
+// {
+//   for(j=0;j<brr.length;j++)
+//   {
+//     if(arr[i]==brr[j])
+//     {
+//       // let k=j
+//       // while(k<brr.length)
+//       // {
+//       //   brr[k]=brr[k+1];
+//       //   k++
+//       // }
+//       // brr.length--
+//       // c=1
+//       c[i]=brr[j];
+//       // c+=1
+//     }
+//     // else{
+//     // c=c
+//     // }
+//   }
+
+// }
+// if(c==0)
+// {
+//   console.log("no");
+// }
+// else if(c==1)
+// {
+//   console.log("yes");
+// }
+// else{
+//   console.log("no");
+// }
+// console.log(c);
+
+
+
+
+
+
